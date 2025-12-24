296d40b7d443ecdc79f2c53a60a3b2eb3b35093e
Mumbai <zeessuzeessu84@gmail.com>
2025-12-25 00:11:44 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,16 @@
 
+const hre = require("hardhat");
+
+async function main() {
+  const Todo = await hre.ethers.getContractFactory("TodoList");
+  const todo = await Todo.deploy();
+
+  await todo.waitForDeployment();
+
+  console.log("TodoList Contract deployed to:", await todo.getAddress());
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
