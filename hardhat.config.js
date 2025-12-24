fd1872cb03399e114d25a08d55003d8c6a265097
Mumbai <zeessuzeessu84@gmail.com>
2025-12-25 00:45:22 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-hhbgghbhhhhyygygggyffggrequire("@nomicfoundation/hardhat-toolbox");
+ghghhbgghbhhhhyygygggyffggrequire("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
